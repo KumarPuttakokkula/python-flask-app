8caad013fb81f67713d68ccc14d3492d7f3c249e
Kumar Puttakokkula <kumar.puttakokkula@gmail.com>
2026-07-21 21:57:58 -0500

    fix: 6

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def root():
 @app.route("/app/v1/info")
 def details():
     return jsonify({
-        "message": "You've developed and deployed this Python Flask App from ArgoCD !!.",
+        "message": "You've developed and deployed this Python Flask App from ArgoCD !!",
         "hostname": socket.gethostname(),
         "time": datetime.datetime.now().strftime("%I:%M:%S%p on %B %d %Y"),
         "deployed_on": "Kubernetes"
